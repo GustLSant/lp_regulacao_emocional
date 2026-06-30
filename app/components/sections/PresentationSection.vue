223abb674339a0efe2b3b374cfeb1d0d197d2986
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-06-30 16:50:57 -0300

    refactor: removed video displayer + changed cta button

--- app/components/sections/PresentationSection.vue
+++ app/components/sections/PresentationSection.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-    import Button from '../common/Button.vue';
-    import VideoDisplayer from '../common/VideoDisplayer.vue';
+    import CTAButton from '../common/CTAButton.vue';
 </script>
 
 
 <template>
-    <section class="flex flex-col items-center justify-center gap-8 py-20 px-4 lg:px-20 bg-white text-center">
+    <section class="flex flex-col items-center justify-center gap-10 py-20 px-4 lg:px-20 bg-white text-center">
 
         <p class="text-lg tracking-wider font-light">A regulação emocional é um processo fundamental para uma prática clínica efetiva e acolhedora</p>
 
-        <VideoDisplayer url="https://www.youtube.com/watch?v=u31qwQUeGuM" />
+        <!-- <VideoDisplayer url="https://www.youtube.com/watch?v=u31qwQUeGuM" /> -->
 
         <div class="flex flex-col md:flex-row items-center gap-4 text-start shadow-md beige-card">
             <div class="bg-light-green rounded-full p-2 h-[48px]">
@@ -33,9 +32,7 @@
             <p class="text-sm text-left tracking-wider">Este curso é uma oportunidade de sair de enfoques superficiais, pouco científicos e pouco eficazes</p>
         </div>
 
-        <Button color="orange" start-icon-name="mdi:book-open-page-variant-outline" class="bg-orange">
-            Quero adquirir agora
-        </Button>
+        <CTAButton />
 
         <!-- <div class="flex items-center gap-2 p-2 text-start text-sm rounded-md border border-black/15">
             <Icon name="mdi:information-variant-circle-outline" size="24" class="opacity-80" />
